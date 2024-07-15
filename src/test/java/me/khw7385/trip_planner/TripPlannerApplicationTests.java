6c0720d3daea22876cb278f248e7f7539ab0f8ad
hyunwon <khw73850@gmail.com>
2024-07-15 16:18:47 +0900

    init: 초기화

---
+++ src/test/java/me/khw7385/trip_planner/TripPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.khw7385.trip_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
